84dcce40e88bdd3e6108a52f5e67c6d40892d146
PhilipeG <philipelazzarini_96@hotmail.com>
2022-05-16 20:50:57 -0300

    commit final

---
+++ calculadora/operacoes.js
@@ -0,0 +1,26 @@
+module.exports = {
+    
+    subtracao: (num1, num2) => {
+      return num1 - num2;
+    },
+
+    soma: (num1, num2) => {
+        return num1 + num2;
+      },
+
+    divisao: (num1, num2) => {
+        return num1 / num2;
+      },
+  
+    multiplicacao: (num1, num2) => {
+      return num1 * num2;
+    },
+
+    porcentagem: (num1) => {
+      return num1 / 100;
+    },
+
+    raizQuadrada: (num1) => {
+      return result = Math.sqrt(num1)
+    }
+  };
---
+++ test/operacoes.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const operacoes = require('../calculadora/operacoes');
+
+const assert = chai.assert;
+
+describe("Testes unitários de uma calculadora", () => {
+  it("Teste: Soma de 2 números", () => {
+    assert.equal(operacoes.soma(20, 20), 40);
+  });
+
+  it("Teste: Subtração de 2 números", () => {
+    assert.equal(operacoes.subtracao(25, 5), 20);
+  });
+
+  it("Teste: Multiplicação de 2 números", () => {
+    assert.equal(operacoes.multiplicacao(9, 9), 81);
+  });
+
+  it("Teste: Divisão de 2 números", () => {
+    assert.equal(operacoes.divisao(100, 10), 10);
+  });
+
+  it("Teste: Cálculo de porcentagem", () => {
+      assert.equal(operacoes.porcentagem(200, 100), 2)
+  });
+
+  it("Teste: Cálculo de raiz quadrada", () => {
+      assert.equal(operacoes.raizQuadrada(81), 9)
+  })
+});
