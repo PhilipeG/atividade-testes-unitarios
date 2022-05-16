56c163f2c3f7a80c1674a1677313544e43d6d402
PhilipeG <philipelazzarini_96@hotmail.com>
2022-05-16 20:17:16 -0300

    first commit

---
+++ operacoes.js
@@ -0,0 +1,22 @@
+module.exports = {
+    
+    subtracao: (num1, num2) => {
+      return num1 - num2;
+    },
+
+    soma: (num1, num2) => {
+        return num1 + num2;
+      },
+
+    divisao: (num1, num2) => {
+        return num1 / num2;
+      },
+  
+    multiplicacao: (num1, num2) => {
+      return num1 * num2;
+    },
+
+    porcentagem: (num1) => {
+      return num1 / 100;
+    }
+  };
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Atividade realizada no módulo de Testes unitários e noções de teste automatizados para o programa de estágio da iblue 
